3320c6b81edb4c47e0ff47610dd13396b3b61863
brianrt <brianrt23@gmail.com>
2017-03-01 15:42:03 -0600

    Added index component to illustrate how to implement templateUrl

---
+++ app/index/index.component.ts
@@ -0,0 +1,11 @@
+import {Component} from '@angular/core';
+
+@Component({
+	selector: 'app-index',
+	templateUrl: './app/index/index.component.html'
+})
+export class IndexComponent {
+	constructor() {
+		// TODO
+	}
+}
