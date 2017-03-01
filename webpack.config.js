910d71d1526ee3a0e8877f73d508bf4dec86631d
brianrt <brianrt23@gmail.com>
2017-03-01 15:43:20 -0600

    Upgraded webpack to version 2.x

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,15 @@
-var webpack = require("webpack");
+var webpack = require("webpack"),
+  path = require('path');
+
+const ccpOptions = {
+  name: 'vendor',
+
+  filename: './dist/vendor.bundle.js'
+};
+
+function root(__path) {
+  return path.join(__dirname, __path);
+}
 
 module.exports = {
   entry: {
@@ -10,19 +21,27 @@ module.exports = {
     filename: "./dist/[name].bundle.js"
   },
   resolve: {
-    extensions: ['', '.ts', '.js']
+    extensions: ['.ts', '.js']
   },
   devtool: 'source-map',
   module: {
-    loaders: [
+    rules: [
       {
         test: /\.ts/,
-        loaders: ['ts-loader'],
+        loader: 'ts-loader',
         exclude: /node_modules/
       }
     ]
   },
   plugins: [
-    new webpack.optimize.CommonsChunkPlugin(/* chunkName= */"vendor", /* filename= */"./dist/vendor.bundle.js")
+    new webpack.optimize.CommonsChunkPlugin(ccpOptions),
+
+    // Takes care of warnings described at https://github.com/angular/angular/issues/11580
+    new webpack.ContextReplacementPlugin(
+      // The (\\|\/) piece accounts for path separators in *nix and Windows
+      /angular(\\|\/)core(\\|\/)(esm(\\|\/)src|src)(\\|\/)linker/,
+      root('./src'), // location of your src
+      { }
+    )
   ]
 }
